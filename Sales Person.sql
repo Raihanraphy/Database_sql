3cc8f463156dbf6de6469858eab1e74fd08feed3
Raihan Rahman <raihanraphy@gmail.com>
2025-06-22 00:15:37 +0600

    Create Sales Person.sql

---
+++ Sales Person.sql
@@ -0,0 +1,12 @@
+select s.name
+from salesperson as s
+where s.sales_id not in 
+(
+    select distinct o.sales_id
+    from orders as o
+    left join
+    company as c
+    on 
+    o.com_id=c.com_id
+    where c.name='RED'
+)
